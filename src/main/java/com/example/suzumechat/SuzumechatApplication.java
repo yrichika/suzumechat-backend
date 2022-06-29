52de7770ca8750ae832439868f67b44489de6896
yosuke <68413973+yrichika@users.noreply.github.com>
2022-06-30 07:01:16 +0900

    first commit

---
+++ src/main/java/com/example/suzumechat/SuzumechatApplication.java
@@ -0,0 +1,13 @@
+package com.example.suzumechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuzumechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuzumechatApplication.class, args);
+	}
+
+}
