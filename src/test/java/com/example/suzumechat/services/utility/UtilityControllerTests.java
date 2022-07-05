83d218f020fc1c750322d0db2985d1c71ac90e49
yosuke <68413973+yrichika@users.noreply.github.com>
2022-07-06 07:43:16 +0900

    #6 created healthcheck test.

---
+++ src/test/java/com/example/suzumechat/services/utility/UtilityControllerTests.java
@@ -0,0 +1,34 @@
+package com.example.suzumechat.services.utility;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import com.example.suzumechat.config.SecurityConfig;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+
+@SpringJUnitConfig
+@ContextConfiguration(classes = SecurityConfig.class)
+@Import(UtilityController.class)
+@WebMvcTest(controllers = UtilityController.class)
+public class UtilityControllerTests {
+    
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void testing_healthcheck_should_just_return_text_1() throws Exception
+    {
+        this.mvc.perform(get("/healthcheck").accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isOk())
+            .andExpect(content().string("1"));
+    }
+}
