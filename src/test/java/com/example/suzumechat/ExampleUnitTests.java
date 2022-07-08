eebdb0fcfd2f843f47d57943ae146172986df46b
yosuke <68413973+yrichika@users.noreply.github.com>
2022-07-08 22:28:06 +0900

    a very minor fix

--- src/test/java/com/example/suzumechat/ExampleUnitTests.java
+++ src/test/java/com/example/suzumechat/ExampleUnitTests.java
@@ -3,11 +3,13 @@ package com.example.suzumechat;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
 import com.example.suzumechat.testconfig.TestConfig;
 
 import lombok.val;
 
-@TestConfiguration
+@SpringJUnitConfig
 @Import(TestConfig.class)
 public class ExampleUnitTests {
     @Test
