ca35ea49dbe5ac2d188841a1ce5e34b9cc6430e1
yosuke <68413973+yrichika@users.noreply.github.com>
2022-07-11 21:24:52 +0900

    minor configuration changes

--- src/main/java/com/example/suzumechat/config/SecurityConfig.java
+++ src/main/java/com/example/suzumechat/config/SecurityConfig.java
@@ -5,12 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
     
+    // originally `Password` object
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
--- src/test/java/com/example/suzumechat/testconfig/TestConfig.java
+++ src/test/java/com/example/suzumechat/testconfig/TestConfig.java
@@ -4,7 +4,6 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 
 import com.example.suzumechat.testutil.random.TestRandom;
-import com.example.suzumechat.testutil.random.pattern.RandomString;
 import com.example.suzumechat.testutil.stub.factory.ChannelFactory;
 import com.example.suzumechat.testutil.stub.factory.GuestFactory;
 
@@ -25,6 +24,4 @@ public class TestConfig {
     public TestRandom testRandom() {
         return new TestRandom();
     }
-
-
 }
