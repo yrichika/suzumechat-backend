6939e45f24b17d43fd5a78e3f533fe449dd1c353
yosuke <68413973+yrichika@users.noreply.github.com>
2022-07-16 17:12:43 +0900

    csrf and cors settings. csrf disabled

--- src/main/java/com/example/suzumechat/config/SecurityConfig.java
+++ src/main/java/com/example/suzumechat/config/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.example.suzumechat.config;
 
+import org.springframework.web.filter.CorsFilter;
+import org.apache.http.client.methods.HttpOptions;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -31,7 +40,36 @@ public class SecurityConfig {
         http.authorizeHttpRequests(auth -> auth
             .antMatchers("/").permitAll()
         );
+        http.csrf().disable();
+        // Cookie based CSRF token FIXME: not working
+        // http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+        
 
         return http.build();
     }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+
+        CorsConfiguration config = new CorsConfiguration();
+        // config.setAllowCredentials(true);
+
+        // Access-Control-Allow-Origin
+        // config.addAllowedOrigin("http://localhost:3000");
+        config.addAllowedOrigin(CorsConfiguration.ALL);
+        // Access-Control-Allow-Methods
+        config.addAllowedMethod(CorsConfiguration.ALL);
+        // Access-Control-Allow-Headers
+        config.addAllowedHeader(CorsConfiguration.ALL);
+        // 全部許可する設定にする
+        // config.applyPermitDefaultValues();
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<CorsFilter>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+
 }
--- src/main/java/com/example/suzumechat/service/utility/UtilityController.java
+++ src/main/java/com/example/suzumechat/service/utility/UtilityController.java
@@ -1,6 +1,8 @@
 package com.example.suzumechat.service.utility;
 
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,10 @@ public class UtilityController {
     public int healthcheck() {
         return 1;
     }
+
+    // TODO:
+    // @GetMapping("/csrfToken")
+    // public CsrfToken csrfToken(CsrfToken token) {
+  	//     return token;
+    // }
 }
