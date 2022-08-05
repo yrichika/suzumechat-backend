c048ba5f17cd8bc4415f5cb9efa683b8591e94f6
yosuke <68413973+yrichika@users.noreply.github.com>
2022-08-05 13:37:49 +0900

    application setting changes

--- src/main/java/com/example/suzumechat/SuzumechatApplication.java
+++ src/main/java/com/example/suzumechat/SuzumechatApplication.java
@@ -2,8 +2,10 @@ package com.example.suzumechat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:settings.properties")
 public class SuzumechatApplication {
 
 	public static void main(String[] args) {
