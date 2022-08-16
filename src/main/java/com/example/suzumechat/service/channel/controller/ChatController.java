c608ac042d7e7481e44057f058e4c3d148b8c5d6
yosuke <68413973+yrichika@users.noreply.github.com>
2022-08-17 08:47:12 +0900

    #13 skipping the tests temporarily. Implementation of the chat functionality for host is done for now.

--- src/main/java/com/example/suzumechat/service/channel/controller/ChatController.java
+++ src/main/java/com/example/suzumechat/service/channel/controller/ChatController.java
@@ -1,26 +1,18 @@
 package com.example.suzumechat.service.channel.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.suzumechat.service.channel.ChannelService;
 import com.example.suzumechat.service.channel.exception.HostUnauthorizedException;
 
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@Slf4j
 public class ChatController {
 
     @Autowired
@@ -40,7 +32,7 @@ public class ChatController {
     public void receiveAndBroadcast(
         @DestinationVariable("hostChannelToken") final String hostChannelToken,
         @Payload final String messageEnc,
-        SimpMessageHeaderAccessor headerAccessor
+        final SimpMessageHeaderAccessor headerAccessor
     ) throws Exception {
         val hostId = headerAccessor.getSessionAttributes().get("hostId").toString();
 
--- src/test/java/com/example/suzumechat/service/channel/controller/ChatControllerTests.java
+++ src/test/java/com/example/suzumechat/service/channel/controller/ChatControllerTests.java
@@ -2,12 +2,16 @@ package com.example.suzumechat.service.channel.controller;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
@@ -21,9 +25,12 @@ import org.springframework.messaging.simp.stomp.StompHeaders;
 import org.springframework.messaging.simp.stomp.StompSession;
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
@@ -51,42 +58,39 @@ import com.example.suzumechat.config.WebSocketConfig;
 import com.example.suzumechat.service.channel.ChannelService;
 import com.example.suzumechat.testconfig.TestConfig;
 import com.example.suzumechat.testutil.random.TestRandom;
-import com.example.suzumechat.utility.Random;
 
 import lombok.*;
 
 @Import({TestConfig.class})
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @MockitoSettings
 public class ChatControllerTests {
-    @MockBean
-    private ChannelService service;
 
     @Autowired
     private Environment env;
-
     @Autowired
     private TestRandom random;
-
-    private CompletableFuture<String> completableFuture;
+    @MockBean
+    private ChannelService service;
 
     @LocalServerPort
     public int port;
 
-    // Referencing: but it doen't work
-    // https://medium.com/@MelvinBlokhuijzen/spring-websocket-endpoints-integration-testing-180357b4f24c
-    @Disabled // TODO:
+    private CompletableFuture<String> completableFuture;
+
+    // REF: https://medium.com/@MelvinBlokhuijzen/spring-websocket-endpoints-integration-testing-180357b4f24c
+    @Disabled // TODO: This test is incomplete. I have no idea how to test WebSocket functionalities.
     @Test
     public void receiveAndBroadcast_should_receive_message_and_broadcast_to_host_and_guests() throws Exception {
 
+
         WebSocketStompClient client = new WebSocketStompClient(new SockJsClient(createTransportClient()));
         client.setMessageConverter(new MappingJackson2MessageConverter());
-        val url = "http://localhost:" + port + env.getProperty("ws.entry-point");
-        // System.out.println(port);
-        // // FIXME: なぜか`/info`が後についてちゃんとしたURLにならない
-        StompSession session = client.connect(url, new StompSessionHandlerAdapter() {})
-            .get(1, TimeUnit.SECONDS);
-        
+        val url = "ws://localhost:" + port + env.getProperty("ws.entry-point");
+
+        // StompSession session = client.connect(url, new StompSessionHandlerAdapter() {})
+        //     .get(1, TimeUnit.SECONDS);
+
         // val hostChannelToken = random.string.alphanumeric();
         // val guestChannelToken = random.string.alphanumeric();
 
@@ -99,8 +103,6 @@ public class ChatControllerTests {
         // val message = completableFuture.get(10, TimeUnit.SECONDS);
 
         // assertThat(message).isNotEmpty();
-
-        assertThat(true).isTrue();
     }
 
     private List<Transport> createTransportClient() {
